8b81593200d0638ff3cb093610eed20a08e5bc59
dan-holmes <dh@danielholmes.com>
2021-01-20 20:54:32 +0000

    Add basic addPiece method

--- src/Board.java
+++ src/Board.java
@@ -1,14 +1,26 @@
 
 public class Board {
-	int[] pieces = {0, 0, 0, 0};
+	private static final int COLUMNS = 7;
+	private static final int ROWS = 6;
+	
+	int[][] pieces;
 	
 	public Board() {
-		
+		pieces = new int[7][6];
+	}
+	
+	public void addPiece(int column, int player) {
+		pieces[column][0] = player;
 	}
 	
 	public void render() {
-		for(int i = 0; i < pieces.length; i++) {
-			System.out.print(pieces[i]);
+		for(int j = 0; j < ROWS; j++) {
+			for(int i = 0; i < COLUMNS; i++) {
+				int row = ROWS - 1 - j ;
+				int column = i;
+				System.out.print(pieces[column][row]);
+			}
+			System.out.print("\n");
 		}
 	}
 }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,8 @@
 class Connect4 {
 	public static void main(String args[]) {
 		Board board = new Board();
+		board.addPiece(3, 1);
+		board.addPiece(4, 2);
 		board.render();
 	}
 }
